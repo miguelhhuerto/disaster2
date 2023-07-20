80517694a747bdd66d304106f1ae70385f5eea54
miguelhhuerto <miguelhhuerto@gmail.com>
2023-07-20 14:58:38 +0800

    Added Current User Line for Authorization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.user = current_user
     if @post.save
       flash[:notice] = 'Post created successfully'
       redirect_to posts_path
