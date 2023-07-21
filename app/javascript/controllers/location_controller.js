15a09db92bd9f82bb5ed1c11b86e8d6bab29d61a
miguelhhuerto <miguelhhuerto@gmail.com>
2023-07-21 16:39:24 +0800

    Added Barangay
    Added Barangay to Philippine Address Selector

--- app/javascript/controllers/location_controller.js
+++ app/javascript/controllers/location_controller.js
@@ -1,7 +1,7 @@
 import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ['selectedRegionId', 'selectedProvinceId','selectCityId']
+    static targets = ['selectedRegionId', 'selectedProvinceId','selectedCityId', 'selectBarangayId']
 
     fetchProvinces() {
         let target = this.selectedProvinceIdTarget
@@ -24,12 +24,31 @@ export default class extends Controller {
     }
 
     fetchCities(){
-        let target = this.selectCityIdTarget
+        let target = this.selectedCityIdTarget
         $(target).empty();
 
         $.ajax({
             type: 'GET',
             url: '/api/v1/provinces/' + this.selectedProvinceIdTarget.value + '/cities',
+            dataType: 'json',
+            success: (response) => {
+                console.log(response)
+                $.each(response, function (index, record) {
+                    let option = document.createElement('option')
+                    option.value = record.id
+                    option.text = record.name
+                    target.appendChild(option)
+                })
+            }
+        })
+    }
+    fetchBarangays(){
+        let target = this.selectBarangayIdTarget
+        $(target).empty();
+
+        $.ajax({
+            type: 'GET',
+            url: '/api/v1/cities/' + this.selectedCityIdTarget.value + '/barangays',
             dataType: 'json',
             success: (response) => {
                 console.log(response)
