7a68ccee25a769979afdf1161aca15332460d5bb
miguelhhuerto <miguelhhuerto@gmail.com>
2023-07-21 18:09:18 +0800

    Added User Reference to Comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_21_074711) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_21_100711) do
   create_table "address_barangays", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "city_id"
     t.string "code"
@@ -56,7 +56,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_21_074711) do
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "post_category_ships", charset: "utf8mb4", force: :cascade do |t|
