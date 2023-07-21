5e858b73af7d21a81ffeaa2d061d8b572d8fe911
miguelhhuerto <miguelhhuerto@gmail.com>
2023-07-21 16:00:14 +0800

    Added User ID to Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_20_074619) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_21_074711) do
   create_table "address_barangays", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "city_id"
     t.string "code"
@@ -79,10 +79,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_20_074619) do
     t.bigint "address_province_id"
     t.bigint "address_city_id"
     t.bigint "address_barangay_id"
+    t.bigint "user_id"
     t.index ["address_barangay_id"], name: "index_posts_on_address_barangay_id"
     t.index ["address_city_id"], name: "index_posts_on_address_city_id"
     t.index ["address_province_id"], name: "index_posts_on_address_province_id"
     t.index ["address_region_id"], name: "index_posts_on_address_region_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
